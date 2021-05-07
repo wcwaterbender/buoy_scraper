4d45271a5341f19cab3c613444330c50287e3c35
Cornish, Samuel <sam.cornish@karlstorz.com>
2021-05-07 12:02:32 -0700

    scrape buoy urls and save graphs as png images locally

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+
+use scraper::{Html, Selector};
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let res = reqwest::get("https://wavecast.com/buoys/").await?;
+
+    println!("Status: {}", res.status());
+
+    let body = res.text().await?;
+
+    let document = Html::parse_document(&body);
+
+    let selector = Selector::parse("img").unwrap();
+
+    let mut images = vec!();
+
+    for element in document.select(&selector) {
+        match element.value().attr("src") {
+            Some(x) if x.contains("plot_wave") => images.push(x),
+            _x => drop(_x),
+        }
+    }
+    let mut counter = 1;
+    for imgurl in images {
+        let img_bytes = reqwest::get(imgurl).await?;
+        let bytes = img_bytes.bytes().await?;
+        match image::load_from_memory(&bytes){
+            Ok(x) => {
+                let mut filename = String::from("imgs/buoy_");
+                filename+=&counter.to_string();
+                filename += &String::from(".png");
+                match x.save(filename){
+                    Ok(_) => println!("Saved Image"),
+                    Err(_) => println!("failed to save img"),
+                };
+                counter+=1;
+            },
+            Err(x) => println!("no image {}",x),
+        }
+    }
+    Ok(())
+}
